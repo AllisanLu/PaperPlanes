9d5e326aad70d704e209839a2927584c3fbf647f
StormBlesse <gabriel.chen@aztopia.com>
2022-01-03 15:38:29 -0500

    Line dragging while drawing now visible
    Line shows up when dragging

--- Assets/Scripts/Line Drawing/DrawStraightLine.cs
+++ Assets/Scripts/Line Drawing/DrawStraightLine.cs
@@ -28,6 +28,12 @@ public class DrawStraightLine : MonoBehaviour
             startMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         }
+        if(Input.GetMouseButton(0))
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            line.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
+            line.SetPosition(1, new Vector3(mousePos.x, mousePos.y, 0f));
+        }
         if(Input.GetMouseButtonUp(0)) 
         {
             
