7012cb4e6edd4c508c4b88f10d128cd87d749539
StormBlesse <gabriel.chen@aztopia.com>
2022-01-03 15:52:02 -0500

    Line Fat
    Line Fat

--- Assets/Scripts/Line Drawing/DrawStraightLine.cs
+++ Assets/Scripts/Line Drawing/DrawStraightLine.cs
@@ -68,7 +68,7 @@ public class DrawStraightLine : MonoBehaviour
         col.isTrigger = true;
         col.transform.SetParent(currentLine.GetComponent<LineRenderer>().transform);
         float lineLength = Vector3.Distance (startMousePos, mousePos); // length of line
-        col.size = new Vector2(lineLength, 0.25f); // size of collider is set where X is length of line, Y is width of line, Z will be set as per requirement
+        col.size = new Vector2(lineLength, 0.5f); // size of collider is set where X is length of line, Y is width of line, Z will be set as per requirement
         Vector3 midPoint = (startMousePos + mousePos)/2;
         currentLine.transform.position = midPoint;
         col.transform.position = midPoint; // setting position of collider object
