cdee0dd24571001579ce1ef770e6e11e185035c0
Allison Lu <allison.lu@gatech.edu>
2022-01-29 15:40:07 -0500

    Add Comments

--- Assets/Scripts/Entities/Enemies/Balloon.cs
+++ Assets/Scripts/Entities/Enemies/Balloon.cs
@@ -14,11 +14,14 @@ public class Balloon : Enemy
     // Update is called once per frame
     void FixedUpdate()
     {
+        //gets command from its controller
         Vector2 command = behaviorController.GetMove();
         transform.position = command;
 
+        //adds on the windForce if there is any
         rb.AddForce(windForce);
 
+        //decays the windforce
         windForceDecay();
     }
 }
