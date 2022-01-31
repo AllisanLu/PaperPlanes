f4016e45b6d3311ff8244422e149716610db1f9e
Jason <jguo383@gatech.edu>
2022-01-30 21:28:35 -0500

    Added player detection and sprites

--- Assets/Scripts/Entities/Enemies/RainCloud.cs
+++ Assets/Scripts/Entities/Enemies/RainCloud.cs
@@ -8,22 +8,23 @@ public class RainCloud : Enemy
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //gets command from its controller
-        Vector2 command = behaviorController.GetMove();
-        Debug.Log(command);
-        transform.position = command;
 
-        Debug.Log(windForce);
-        //adds on the windForce if there is any
-        rb.AddForce(windForce);
-
-        //decays the windforce
-        windForceDecay();
     }
+    
+    // Triggers when plane is under
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Plane under cloud");
+        }
+        
+    }
+
 }
