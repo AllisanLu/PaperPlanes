55c8b78796abddf2491118db4b1d5448a4dc7607
Zachary Goddard <zgoddard3@gmail.com>
2022-01-31 22:27:33 -0500

    Fine tune wind control

--- Assets/Scripts/Line Drawing/DrawStraightLine.cs
+++ Assets/Scripts/Line Drawing/DrawStraightLine.cs
@@ -14,6 +14,7 @@ public class DrawStraightLine : MonoBehaviour
 
     public Vector3 endMousePos;
     public List<Vector2> positions;
+    public float strength;
     void Start()
     {
         
@@ -62,7 +63,9 @@ public class DrawStraightLine : MonoBehaviour
     void addColliderToLine()
     {
         GameObject wind = new GameObject("WindCollider");
-        wind.AddComponent<WindCurrent>();
+        WindCurrent windcurrent = wind.AddComponent<WindCurrent>();
+
+        windcurrent.force = (mousePos - startMousePos).magnitude * strength;
 
         BoxCollider2D col = wind.AddComponent<BoxCollider2D> ();
         col.isTrigger = true;
