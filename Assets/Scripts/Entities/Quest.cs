526e2986d7b11359963fa44d89180a1808ea8830
saigautambonam@gmail.com <saigautambonam@gmail.com>
2022-02-16 11:42:27 -0500

    Remove Monobehavior from Quest

--- Assets/Scripts/Entities/Quest.cs
+++ Assets/Scripts/Entities/Quest.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Quest : MonoBehaviour
+public class Quest
 {
     public string QuestDescription;
     public bool current;
