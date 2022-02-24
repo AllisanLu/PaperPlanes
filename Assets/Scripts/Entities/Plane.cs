5bf64432056c1093855d2ccb3dbf05a5654e0d5b
kinshukphalke <phalkekinshuk@gmail.com>
2022-02-24 16:21:21 -0500

    Added Relaunch Scene

--- Assets/Scripts/Entities/Plane.cs
+++ Assets/Scripts/Entities/Plane.cs
@@ -179,18 +179,6 @@ public class Plane : Entity
 			}
 		}
 	}
-
-	IEnumerator ExampleCoroutine()
-    {
-        //Print the time of when the function is first called.
-        Debug.Log("Started Coroutine at timestamp : " + Time.time);
-
-        //yield on a new YieldInstruction that waits for 5 seconds.
-        yield return new WaitForSeconds(5);
-
-        //After we have waited 5 seconds print the time again.
-        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
-    }
 }
 
 
