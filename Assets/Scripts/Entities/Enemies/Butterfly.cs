5799b3681250457d411c5d7436091e9bd6b61510
Allison Lu <allison.lu@gatech.edu>
2022-03-02 23:07:41 -0500

    Add Butterfly Enemy

---
+++ Assets/Scripts/Entities/Enemies/Butterfly.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Butterfly : Enemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 command = behaviorController.GetMove();
+        transform.position = command;
+    }
+
+    // Triggers when plane is hitting the borb
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.collider.GetComponent<Plane>().takeDamage(damage);
+        }
+    }
+}
