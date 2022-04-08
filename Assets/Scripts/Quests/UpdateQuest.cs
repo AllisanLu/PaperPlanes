681c446f77234d136448050cb5cc5472511679d7
Allison Lu <allison.lu@gatech.edu>
2022-04-08 09:49:52 -0400

    Comment out unworking changes

--- Assets/Scripts/Quests/UpdateQuest.cs
+++ Assets/Scripts/Quests/UpdateQuest.cs
@@ -6,9 +6,9 @@ using UnityEngine.UI;
 public class UpdateQuest : MonoBehaviour
 {
     [SerializeField]
-    //private Text questDisplay;
+    private Text questDisplay;
 
-    public GameObject questDisplay;
+    //public GameObject questDisplay;
     
     private bool isOn = true;
     // Start is called before the first frame update
@@ -21,31 +21,34 @@ public class UpdateQuest : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q)) {
-            /*            // toggle visibility of questDisplay
-                        Color color = questDisplay.color;
-                        if (isOn) {
-                            color.a = 0f;
-                        } else {
-                            color.a = 1f;
-                        }
-                        isOn = !isOn;
-                        questDisplay.color = color;*/
+            // toggle visibility of questDisplay
+            Color color = questDisplay.color;
+            if (isOn)
+            {
+                color.a = 0f;
+            }
+            else
+            {
+                color.a = 1f;
+            }
             isOn = !isOn;
-            questDisplay.SetActive(isOn);
-            print(questDisplay.GetComponent<Text>().text);
+            questDisplay.color = color;
+/*            isOn = !isOn;
+            questDisplay.SetActive(isOn);*/
         }
         // Display all current quests in Text UI element
         ArrayList quests = QuestSystem.GetCurrentQuests();
-        questDisplay.GetComponent<Text>().text = "Messages:";
+        //questDisplay.GetComponent<Text>().text = "Messages:";
+        string text = "Messages: ";
         foreach (Quest q in quests) {
             // Add quest description per line
-            // text += q.QuestDescription + "\n";
-            GameObject go = new GameObject();
+             text += q.QuestDescription + "\n";
+/*            GameObject go = new GameObject();
             go.AddComponent<Text>();
             go.GetComponent<Text>().text = q.QuestDescription;
-            go.transform.SetParent(questDisplay.transform, false);
+            go.transform.SetParent(questDisplay.transform, false);*/
         }
         // Set UI text to quest description text
-        //questDisplay.text = text;
+        questDisplay.text = text;
     }
 }
