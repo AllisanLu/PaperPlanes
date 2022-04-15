4dfc28a02ec8297643e7c13a9ee1ddcd5fea33d5
StormBlesse <gabriel.chen@aztopia.com>
2022-04-15 00:18:20 -0400

    Add SFX
    Add SFX

--- Assets/Scripts/Entities/Enemies/Dog.cs
+++ Assets/Scripts/Entities/Enemies/Dog.cs
@@ -26,12 +26,17 @@ public class Dog : Enemy
     private float runX;
 
     private bool reverse = false;
+    private bool barked = false;
+    public static FMOD.Studio.EventInstance Bark;
 
     public Plane plane;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        Bark = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/Dog");
+
         rb = GetComponent<Rigidbody2D>();   
         behaviorController = GetComponent<DogController>();
 
@@ -44,6 +49,15 @@ public class Dog : Enemy
         rb.gravityScale = 0;
     }
 
+    void Update()
+    {
+        if(GetComponent<Renderer>().isVisible && !barked)
+        {   
+            barked = true;
+            Bark.start();
+
+        }
+    }
     public override void Move()
     {   
         if (plane.gameObject.transform.position.x + startoffset > transform.position.x && plane.gameObject.transform.position.x < transform.position.x + endoffset) {
